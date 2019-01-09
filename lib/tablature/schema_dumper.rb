f93d77eef61fd29b81f618e97257317b027392ea
Aliou Diallo <code@aliou.me>
2019-01-09 08:42:57 +0100

    wip

--- lib/tablature/schema_dumper.rb
+++ lib/tablature/schema_dumper.rb
@@ -7,12 +7,43 @@ module Tablature
         (ActiveRecord::SchemaDumper.ignore_tables || []) + partitions
 
       super
+      partitioned_tables(stream)
+    end
+
+    def partitioned_tables(stream)
+      return stream unless dumpable_partitioned_tables.any?
+
+      stream.puts
+
+      dumpable_partitioned_tables.each do |partitionned_table|
+        dump_partioned_table(partitionned_table, stream)
+      end
+
+      stream
     end
 
     private
 
+    def dump_partioned_table(partitioned_table, main_stream)
+      # Call the original method to populate our custom stream
+      stream = StringIO.new
+      table(partitioned_table.name, stream)
+
+      content = stream.tap(&:rewind).read
+                      .gsub(
+                        /create_table.*/,
+                        "create_list_partition \"#{partitioned_table.name}\, partition_key: \"#{partitioned_table.partition_key}\" do"
+                      )
+
+      binding.pry
+
+      # main_stream.print(content)
+
+      main_stream
+    end
+
     def dumpable_partitioned_tables
-      Tablature.database.partitioned_tables
+      Tablature.database.partitioned_tables.sort
     end
 
     def partitions
