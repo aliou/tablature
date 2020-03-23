9efd6561deea414a31a1288c0624ebaaedc36377
Aliou Diallo <code@aliou.me>
2020-03-23 21:40:56 +0100

    Don't create a class in test.

--- spec/tablature/model_spec.rb
+++ spec/tablature/model_spec.rb
@@ -12,123 +12,146 @@ RSpec.describe Tablature::Model, :database do
 
   describe Tablature::Model::ClassMethods do
     describe '#tablature_partition' do
-      after do
-        Event.remove_instance_variable(:@tablature_partition)
-      end
+      # after(:each) do
+      #   Event.remove_instance_variable(:@tablature_partition)
+      # end
 
       context 'without a custom name' do
-        it 'returns the tablature partition' do
-          class Event < ActiveRecord::Base
+        let(:model) do
+          Class.new(ActiveRecord::Base) do
             include Tablature::Model
+            self.table_name = 'events'
           end
+        end
 
-          Event.send(:setup_partition, 'events')
+        it 'returns the tablature partition' do
+          model.send(:setup_partition, 'events')
           connection = ActiveRecord::Base.connection
           connection.execute <<-SQL
             CREATE TABLE "events" ("id" bigserial NOT NULL) PARTITION BY LIST ("id");
           SQL
 
-          tablature_partition = Event.tablature_partition
+          tablature_partition = model.tablature_partition
           expect(tablature_partition).to be_a Tablature::PartitionedTable
           expect(tablature_partition.name).to eq('events')
         end
       end
 
       context 'with a custom name' do
-        it 'returns the tablature partition' do
-          class Event < ActiveRecord::Base
+        let(:model) do
+          Class.new(ActiveRecord::Base) do
             include Tablature::Model
+            self.table_name = 'events'
           end
-          Event.send(:setup_partition, 'events_2019')
+        end
+
+        it 'returns the tablature partition' do
+          model.send(:setup_partition, 'events_2019')
           connection = ActiveRecord::Base.connection
           connection.execute <<-SQL
             CREATE TABLE "events_2019" ("id" bigserial NOT NULL) PARTITION BY LIST ("id");
           SQL
 
-          tablature_partition = Event.tablature_partition
+          tablature_partition = model.tablature_partition
           expect(tablature_partition).to be_a Tablature::PartitionedTable
           expect(tablature_partition.name).to eq('events_2019')
         end
       end
 
       context 'when there are no partitioned table' do
-        it 'raises an error' do
-          class Event < ActiveRecord::Base; end
+        let(:model) do
+          Class.new(ActiveRecord::Base) do
+            include Tablature::Model
+            self.table_name = 'events'
+          end
+        end
 
-          expect { Event.tablature_partition }.to raise_error(Tablature::MissingPartition)
+        it 'raises an error' do
+          expect { model.tablature_partition }.to raise_error(Tablature::MissingPartition)
         end
       end
     end
 
     describe 'partitioned?' do
-      it 'is true when partitioned' do
-        class Event < ActiveRecord::Base
+      let(:model) do
+        Class.new(ActiveRecord::Base) do
           include Tablature::Model
+          self.table_name = 'events'
         end
-        Event.send(:setup_partition, 'events')
+      end
+
+      it 'is true when partitioned' do
+        model.send(:setup_partition, 'events')
         connection = ActiveRecord::Base.connection
         connection.execute <<-SQL
           CREATE TABLE "events" ("id" bigserial NOT NULL) PARTITION BY LIST ("id");
         SQL
 
-        expect(Event.partitioned?).to be true
+        expect(model.partitioned?).to be true
       end
 
       it 'is false when not partitioned' do
-        class Event < ActiveRecord::Base
-          include Tablature::Model
-        end
-
-        expect(Event.partitioned?).to be false
+        expect(model.partitioned?).to be false
       end
     end
 
     describe '#partitions' do
-      it 'delegates to the partition table object' do
-        class Event < ActiveRecord::Base
+      let(:model) do
+        Class.new(ActiveRecord::Base) do
           include Tablature::Model
+          self.table_name = 'events'
         end
+      end
+
+      it 'delegates to the partition table object' do
+        # class Event < ActiveRecord::Base
+        #   include Tablature::Model
+        # end
         tablature_partition_double = double(Tablature::PartitionedTable)
-        allow(Event).to receive(:tablature_partition).and_return(tablature_partition_double)
+        allow(model).to receive(:tablature_partition).and_return(tablature_partition_double)
 
         expect(tablature_partition_double).to receive(:partitions)
-        Event.partitions
+        model.partitions
       end
     end
 
     describe '#partition_key' do
-      it 'delegates to the partition table object' do
-        class Event < ActiveRecord::Base
-          list_partition
+      let(:model) do
+        Class.new(ActiveRecord::Base) do
+          include Tablature::Model
         end
+      end
+
+      it 'delegates to the partition table object' do
         tablature_partition_double = double(Tablature::PartitionedTable)
-        allow(Event).to receive(:tablature_partition).and_return(tablature_partition_double)
+        allow(model).to receive(:tablature_partition).and_return(tablature_partition_double)
 
         expect(tablature_partition_double).to receive(:partition_key)
-        Event.partition_key
+        model.partition_key
       end
     end
 
     describe '#partitioning_method' do
-      it 'delegates to the partition table object' do
-        class Event < ActiveRecord::Base
+      let(:model) do
+        Class.new(ActiveRecord::Base) do
           include Tablature::Model
         end
+      end
+
+      it 'delegates to the partition table object' do
         tablature_partition_double = double(Tablature::PartitionedTable)
-        allow(Event).to receive(:tablature_partition).and_return(tablature_partition_double)
+        allow(model).to receive(:tablature_partition).and_return(tablature_partition_double)
 
         expect(tablature_partition_double).to receive(:partitioning_method)
-        Event.partitioning_method
+        model.partitioning_method
       end
     end
+
     describe '.list_partition' do
       let(:model) do
-        Class.new do
+        Class.new(ActiveRecord::Base) do
           include Tablature::Model
-
-          def self.table_name
-            'events'
-          end
+          self.table_name = 'events'
         end
       end
 
@@ -146,12 +169,9 @@ RSpec.describe Tablature::Model, :database do
 
     describe '.range_partition' do
       let(:model) do
-        Class.new do
+        Class.new(ActiveRecord::Base) do
           include Tablature::Model
-
-          def self.table_name
-            'events'
-          end
+          self.table_name = 'events'
         end
       end
 
